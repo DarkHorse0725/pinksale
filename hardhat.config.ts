46783b219ae72d4793b777ce2cbc8ddd90c84072
darkhorse0725 <darktop0112@gmail.com>
2024-05-16 11:26:37 +0900

    deploy presale

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -23,6 +26,20 @@ const config: HardhatUserConfig = {
         }
       }
     ],
+  },
+  networks: {
+    testnet: {
+      url: "https://sepolia.infura.io/v3/52ee1cb21e8a4095aef5f92fa50f70e3",
+      accounts: 
+      process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
